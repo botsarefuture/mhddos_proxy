0efd936e32c78321f74386f397e8aea9fac1db98
botsarefuture <vuoreol@pm.me>
2025-01-13 14:28:14 +0200

    Update script path in start.sh to use relative path for launch_and_keep_max.sh

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@
 # No exceptions are raised
 #
 
-script_path="/home/verso/mhddos_proxy/launch_and_keep_max.sh"
+script_path="./launch_and_keep_max.sh"
 
 while true; do
     if ! pgrep -f "bash $script_path" > /dev/null; then
